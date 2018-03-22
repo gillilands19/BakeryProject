d69ff485e12d226ee844f6f96a631c76640e34de
gillilands19 <gillilands19@gmail.com>
2018-03-21 20:48:00 -0700

    Initial Commit

--- BakeryProject/Controllers/ProductsController.cs
+++ BakeryProject/Controllers/ProductsController.cs
@@ -7,26 +7,13 @@ using BakeryProject.Models;
 
 namespace BakeryProject.Controllers
 {
-    public class HomeController : Controller
+    public class ProductsController : Controller
     {
         BakeryEntities db = new BakeryEntities();
+        // GET: Products
         public ActionResult Index()
         {
             return View(db.Products.ToList());
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
---
+++ BakeryProject/Controllers/RegistrationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryProject.Models;
+
+namespace BakeryProject.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+        BakeryEntities db = new BakeryEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "PersonKey, PersonLastName, PersonFirstName, PersonEmail, PersonPhone, PersonDateAdded" )]Person newPerson)
+        {
+            newPerson.PersonDateAdded = DateTime.Now;
+            db.People.Add(newPerson);
+            db.SaveChanges();
+
+            Message registrationSuccess = new Message();
+            registrationSuccess.MessageText = "Thank you for registering!";
+            return RedirectToAction("Result", registrationSuccess);
+        }
+    }
+}
