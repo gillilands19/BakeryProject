413548ab93f0525ee90a0a3f213ccf1db633160d
gillilands19 <gillilands19@gmail.com>
2018-03-23 18:48:30 -0700

    Changes Navigation Links and Application Title, changes Order Form Approach

---
+++ BakeryProject/Controllers/NewSaleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryProject.Models;
+
+namespace BakeryProject.Controllers
+{
+    public class NewSaleController : Controller
+    {
+
+        BakeryEntities db = new BakeryEntities();
+
+        // GET: NewSale
+        public ActionResult Index()
+        {
+
+            ViewBag.Products = db.Products.ToList();
+
+            ViewBag.ProductKey = new SelectList(db.Products, "ProductKey", "ProductName");
+
+            return View();
+        }
+
+        public ActionResult Receipt()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "SaleDetailKey, SaleKey, ProductKey, SaleDetailPriceCharged, SaleDetailQuantity, SaleDetailDiscount, SaleDetailSaleTaxPercent, SaleDetailEatInTax ")]SaleDetail newSale)
+        {
+            return View();
+        }
+
+    }
+}
